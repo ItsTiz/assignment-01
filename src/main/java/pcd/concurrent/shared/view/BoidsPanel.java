b897e9c4b71f6af0d829b6327eb0a359fceb22bf
Tiziano Vuksan <tiziano.vuksan@studio.unibo.it>
2025-04-13 13:55:15 +0200

    feat: added grid drawing in Panel

--- src/main/java/pcd/concurrent/shared/view/BoidsPanel.java
+++ src/main/java/pcd/concurrent/shared/view/BoidsPanel.java
@@ -14,19 +14,19 @@ import java.util.Random;
 
 public class BoidsPanel extends JPanel {
 
-	private BoidsView view;
-	private BoidsModel model;
+    private BoidsView view;
+    private BoidsModel model;
     private int framerate;
     private final Map<String, String> workerIdentifiers;
 
     public BoidsPanel(BoidsView view, BoidsModel model) {
-    	this.model = model;
-    	this.view = view;
+        this.model = model;
+        this.view = view;
         this.workerIdentifiers = new HashMap<>();
     }
 
     public void setFrameRate(int framerate) {
-    	this.framerate = framerate;
+        this.framerate = framerate;
     }
 
     public void fillUniqueHandlers(List<String> handlers){
@@ -35,31 +35,47 @@ public class BoidsPanel extends JPanel {
             workerIdentifiers.put(handler, String.format("#%06X", r));
         }
     }
-    
+
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         setBackground(Color.WHITE);
-        
+
         var w = view.getWidth();
         var h = view.getHeight();
         var envWidth = model.getWidth();
-        var xScale = w/envWidth;
+        var envHeight = model.getHeight();
+        var xScale = 1;
+        var yScale = 1;
 
         var boids = model.getBoids();
+
+        var cellSize = model.getPerceptionRadius();
+
+        g.setColor(Color.LIGHT_GRAY);
+
+        for (int x = (int) (-envWidth / 2); x <= envWidth / 2; x += cellSize) {
+            int screenX = (int) (w / 2 + x * xScale);
+            g.drawLine(screenX, 0, screenX, h);
+        }
+
+        for (int y = (int) (-envHeight / 2); y <= envHeight / 2; y += cellSize) {
+            int screenY = (int) (h / 2 - y * yScale);
+            g.drawLine(0, screenY, w, screenY);
+        }
 
         for (Boid boid : boids) {
             String hexColor = boid.getCurrentHandler() != null ? workerIdentifiers.get(boid.getCurrentHandler())  : "#269744";
             g.setColor(Color.decode(hexColor));
             var x = boid.getPos().x();
-        	var y = boid.getPos().y();
-        	int px = (int)(w/2 + x*xScale);
-        	int py = (int)(h/2 - y*xScale);
+            var y = boid.getPos().y();
+            int px = (int)(w/2 + x*xScale);
+            int py = (int)(h/2 - y*xScale);
             g.fillOval(px,py, 8, 8);
         }
-        
+
         g.setColor(Color.BLACK);
         g.drawString("Num. Boids: " + boids.size(), 10, 25);
         g.drawString("Framerate: " + framerate, 10, 40);
-   }
+    }
 }
